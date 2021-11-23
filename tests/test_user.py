c596bd0a97b5e31c1954d4f5731334511d60a7c1
Tosin <tosintubi@users.noreply.github.com>
2021-11-23 23:57:29 +0100

    fixing test scripts

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,6 +1,5 @@
 import os
 import json
-import logging
 
 from unittest import TestCase
 from dotenv import load_dotenv
@@ -15,68 +14,67 @@ load_dotenv()
 class TestUser(TestCase):
 
     
-    # def test_token_decode(self):
-    #     """
-    #     Test for decoding token and extracting user information.
-    #     """
+    def test_token_decode(self):
+        """
+        Test for decoding token and extracting user information.
+        """
 
-    #     token = {
-    #         'id_token': os.environ.get("TEST_TOKEN")
-    #                }
+        token = {
+            'id_token': os.environ.get("TEST_TOKEN")
+                   }
     
-    #     decoded_token = decode_token(token['id_token'])
+        decoded_token = decode_token(token['id_token'])
         
-    #     if decode_token != HTTP_400_BAD_REQUEST:
-    #         self.assertEqual(decoded_token['email'], os.environ.get("EMAIL"))
-    #         self.assertEqual(decoded_token['given_name'], 'Yakitabu')
-    #         self.assertEqual(decoded_token['family_name'], 'Project')
+        self.assertEqual(decoded_token['email'], os.environ.get("EMAIL"))
+        self.assertEqual(decoded_token['given_name'], 'Yakitabu')
+        self.assertEqual(decoded_token['family_name'], 'Project')
 
 
-    # def test_valid_login(self):
-    #     """
-    #     Test case covering Valid Login
-    #     """
-    #     token = {
-    #         'id_token': os.environ.get("TEST_TOKEN")
-    #                }
-    #     flask_app = create_app()
+    def test_valid_login(self):
+        """
+        Test case covering Valid Login
+        """
+        token = {
+            'id_token': os.environ.get("TEST_TOKEN")
+                   }
+        flask_app = create_app()
         
 
-    #     with flask_app.test_client() as test_client:
-    #         response = test_client.post('http://localhost:5000/api/v1/user/login/google',
-    #                                     data=json.dumps(token),
-    #                                     content_type='application/json',
-    #                                     )
-    #         assert response.status_code == HTTP_200_OK
+        with flask_app.test_client() as test_client:
+            response = test_client.post('http://localhost:5000/api/v1/user/login/google',
+                                        data=json.dumps(token),
+                                        content_type='application/json',
+                                        )
+            assert response.status_code == HTTP_200_OK
             
 
-    # def test_invalid_login(self):
-    #     """
-    #     Test case covering Bad Request
-    #     """
-    #     token = {'id_token': "5om3hcvjhkct.cyfkukbhckyjsjdjsdsdsdtfghjkghv.vjkfyjujhjctrzrerrezxeszZwerzbxd.InvalidTokeN"}
-    #     flask_app = create_app()
+    def test_invalid_login(self):
+        """
+        Test case covering Bad Request
+        """
+        token = {'id_token': "5om3hcvjhkct.cyfkukbhckyjsjdjsdsdsdtfghjkghv.vjkfyjujhjctrzrerrezxeszZwerzbxd.InvalidTokeN"}
+        flask_app = create_app()
         
        
-    #     with flask_app.test_client() as test_client:
-    #         response = test_client.post('http://localhost:5000/api/v1/user/login/google',
-    #                                     data=json.dumps(token),
-    #                                     content_type='application/json',
-    #                                     )
-    #         assert response.status_code == HTTP_400_BAD_REQUEST
+        with flask_app.test_client() as test_client:
+            response = test_client.post('http://localhost:5000/api/v1/user/login/google',
+                                        data=json.dumps(token),
+                                        content_type='application/json',
+                                        )
+            assert response.status_code == HTTP_400_BAD_REQUEST
 
        
-    # def test_login_get(self):
-    #     """
-    #     Test case covering unsupported METHOD: POST
-    #     """
+    def test_login_get(self):
+        """
+        Test case covering unsupported METHOD: POST
+        """
 
-    #     flask_app = create_app()
+        flask_app = create_app()
 
-    #     with flask_app.test_client() as test_client:
-    #         response = test_client.get('http://localhost:5000/api/v1/user/login/google')
+        with flask_app.test_client() as test_client:
+            response = test_client.get('http://localhost:5000/api/v1/user/login/google')
 
-    #         assert response.status_code == HTTP_405_METHOD_NOT_ALLOWED
+            assert response.status_code == HTTP_405_METHOD_NOT_ALLOWED
             
             
     def test_login_put(self):
@@ -88,8 +86,5 @@ class TestUser(TestCase):
 
         with flask_app.test_client() as test_client:
             response = test_client.get('http://localhost:5000/api/v1/user/login/google')
-            
-            
-            logging.info(os.environ.get("EMAIL"))
-            self.assertEqual("yakitabu.io@gmail.com" == os.environ.get("EMAIL"))
+
             assert response.status_code == HTTP_405_METHOD_NOT_ALLOWED
