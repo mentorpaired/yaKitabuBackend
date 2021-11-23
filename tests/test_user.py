d1dabe052471ca5742901d4a3cead5f718605b21
Tosin <tosintubi@users.noreply.github.com>
2021-11-23 23:41:36 +0100

    fixing test scripts

--- tests/test_user.py
+++ tests/test_user.py
@@ -87,5 +87,6 @@ class TestUser(TestCase):
 
         with flask_app.test_client() as test_client:
             response = test_client.get('http://localhost:5000/api/v1/user/login/google')
-
+            
+            assert "yakitabu.io@gmail.com" == os.environ.get("EMAIL")
             assert response.status_code == HTTP_405_METHOD_NOT_ALLOWED
