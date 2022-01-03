d0e98dd0ae2f4cae929e564e70aa113338e88519
Seunfunmi Adegoke <adegokeseunfunmi1999@gmail.com>
2022-01-03 19:43:01 +0100

    Create migration files

--- migrations/env.py
+++ migrations/env.py
@@ -1,8 +1,10 @@
 from __future__ import with_statement
+
 import logging
 from logging.config import fileConfig
 
 from flask import current_app
+
 from alembic import context
 
 # this is the Alembic Config object, which provides
